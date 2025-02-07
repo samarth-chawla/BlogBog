a8b8c209fdde3e632c14cebe2634be32b1879e69
samarth-chawla <samarthchawla91@gmail.com>
2025-02-08 01:35:15 +0530

    fixes

--- index.js
+++ index.js
@@ -278,7 +278,6 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: 'http://localhost:3000/auth/google/index'
-      // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
       try {
