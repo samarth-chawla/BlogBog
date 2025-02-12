f0fe71a4390b0282853515e046bf2b72ea74d6db
samarth-chawla <167853500+samarth-chawla@users.noreply.github.com>
2025-02-13 01:09:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -277,7 +277,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/index'
+      callbackURL: 'https://blogbogs.onrender.com/auth/google/index'
     },
     async (accessToken, refreshToken, profile, cb) => {
       try {
